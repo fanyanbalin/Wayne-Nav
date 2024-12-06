8b434c61d2a2593932471bb66e55c9ae2a911679
Wayne <way8941@gmail.com>
2024-12-06 11:10:14 +0800

    chore: release 4.6.2

--- js/function.js
+++ js/function.js
@@ -532,8 +532,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：4.6.1
-更新日期：2024-12-02
+版 本 号：4.6.2
+更新日期：2024-12-06
 
 WayneのNav: https://wnav.pages.dev
 Github:  https://github.com/Waynenet/Wayne-Nav
