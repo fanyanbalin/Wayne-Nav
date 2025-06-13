b97e4a6fb299b17d6ea6d45b46602ba01e2dc338
Wayne <way8941@gmail.com>
2025-06-13 15:47:08 +0800

    🧹chore: release v5.2.2

--- js/function.js
+++ js/function.js
@@ -692,8 +692,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：v5.2.1
-更新日期：2025-05-27
+版 本 号：v5.2.2
+更新日期：2025-06-13
 
 WayneのNav: https://nav-26e.pages.dev
 Github:  https://github.com/Waynenet/Wayne-Nav
