00562364c1cb4c26e3b0ea697cde85f2b13c7728
Wayne <way8941@gmail.com>
2025-09-10 16:13:11 +0800

    chore: release v8.1.1

--- js/core.js
+++ js/core.js
@@ -1022,8 +1022,8 @@ function setupFooterInfo() {
 `;
 
     const infoText = `
-版 本 号：v8.1.0
-更新日期：2025-09-09
+版 本 号：v8.1.1
+更新日期：2025-09-10
 
 WayneのNav: https://nav.3301.qzz.io/
 Github:  https://github.com/Waynenet/Wayne-Nav
